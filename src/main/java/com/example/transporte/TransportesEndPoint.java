791b40280146adcc1a2ef9ec5260ca6d7cae9aac
FernandoEspiritu <tiran.sc.f@gmail.com>
2021-04-16 01:58:51 -0500

    primer traquing  todo funcionando

---
+++ src/main/java/com/example/transporte/TransportesEndPoint.java
@@ -0,0 +1,16 @@
+package com.example.transporte;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ws.server.endpoint.annotation.Endpoint;
+import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
+import org.springframework.ws.server.endpoint.annotation.RequestPayload;
+import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
+
+
+@Endpoint
+public class TransportesEndPoint {
+    
+    
+}
